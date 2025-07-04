d90d7ce07353d4408aa6ee10e3de50078711bb94
urabexon <hiroki708.a48uk@gmail.com>
2025-07-04 23:33:56 +0900

    Implement weighted settlement

--- internal/domain/valueobject/yen.go
+++ internal/domain/valueobject/yen.go
@@ -17,3 +17,26 @@ func NewYen(amount int) (Yen, error) {
 	}
 	return Yen(amount), nil
 }
+
+func (y Yen) Int64() int64 {
+	return int64(y)
+}
+
+func (y Yen) String() string {
+	p := message.NewPrinter(language.Japanese)
+	return p.Sprintf("%d円", number.Decimal(y.Int64()))
+}
+
+func (y Yen) MultiplyBy(multiplier int) (Yen, error) {
+	if multiplier < 0 {
+		return 0, errors.New("multiplier cannot be negative")
+	}
+	return Yen(y.Int64() * int64(multiplier)), nil
+}
+
+func (y Yen) CeilDivideBy(divisor int) (Yen, error) {
+	if divisor <= 0 {
+		return 0, fmt.Errorf("divisor cannot be zero or negative (%d / %d)", y.Int64(), divisor)
+	}
+	return Yen((y.Int64() + int64(divisor) - 1) / int64(divisor)), nil
+}
