cc374d88bcd958788ce79c9f4690b2cdd790ad15
urabexon <hiroki708.a48uk@gmail.com>
2025-07-05 01:26:10 +0900

    payment Create

--- internal/infrastructure/handler/command.go
+++ internal/infrastructure/handler/command.go
@@ -27,3 +27,9 @@ type CommandHandler struct {
 	paymentUsecase *usecase.PaymentUsecase
 }
 
+func (h *SlackCommandHandler) handleWarikanCommand(slash slack.SlashCommand) error {
+	eventID := valueobject.NewEventID(slash.ChannelID)
+	payerID := valueobject.NewPayerID(slash.UserID)
+
+	
+}
--- internal/usecase/payment.go
+++ internal/usecase/payment.go
@@ -34,3 +34,38 @@ type SettlementInstruction struct {
 	To     valueobject.PayerID
 	Amount valueobject.Yen
 }
+
+func (u *PaymentUsecase) Create(eventID valueobject.EventID, payerID valueobject.PayerID, amount valueobject.Yen) (*entity.Payment, error) {
+	if eventID.IsUnknown() {
+		return nil, valueobject.NewErrorNotFound("eventID is unknown", nil)
+	}
+	event := &entity.Event{
+		ID: eventID,
+	}
+	if err := u.events.CreateIfNotExists(event); err != nil {
+		return nil, fmt.Errorf("failed to create event: %w", err)
+	}
+
+	if payerID.IsUnknown() {
+		return nil, valueobject.NewErrorNotFound("payerID is unknown", nil)
+	}
+	payer := &entity.Payer{
+		ID:      payerID,
+		EventID: eventID,
+	}
+	if err := u.payers.CreateIfNotExists(payer); err != nil {
+		return nil, fmt.Errorf("failed to create payer: %w", err)
+	}
+
+	payment := &entity.Payment{
+		ID:      valueobject.NewPaymentID(),
+		EventID: eventID,
+		PayerID: payerID,
+		Amount:  amount,
+	}
+	if err := u.payments.Create(payment); err != nil {
+		return nil, fmt.Errorf("failed to create payment: %w", err)
+	}
+
+	return payment, nil
+}
