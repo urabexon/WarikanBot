c935c4cc6f72d365ce901b299fd5f0eba8c4c814
urabexon <hiroki708.a48uk@gmail.com>
2025-07-04 23:23:45 +0900

    id setup

--- internal/domain/valueobject/id.go
+++ internal/domain/valueobject/id.go
@@ -1,1 +1,51 @@
 package valueobject
+
+import (
+	"github.com/google/uuid"
+)
+
+type (
+	EventID   struct{ value string }
+	PayerID   struct{ value string }
+	PaymentID struct{ value uuid.UUID }
+)
+
+func NewEventID(value string) EventID {
+	return EventID{value: value}
+}
+
+func (e EventID) String() string {
+	return e.value
+}
+
+func (e EventID) IsUnknown() bool {
+	return e.value == ""
+}
+
+func NewPayerID(value string) PayerID {
+	return PayerID{value: value}
+}
+
+func (p PayerID) String() string {
+	return p.value
+}
+
+func (p PayerID) IsUnknown() bool {
+	return p.value == ""
+}
+
+func NewPaymentID() PaymentID {
+	return PaymentID{value: uuid.New()}
+}
+
+func NewPaymentIDFromString(value string) (PaymentID, error) {
+	id, err := uuid.Parse(value)
+	if err != nil {
+		return PaymentID{}, err
+	}
+	return PaymentID{value: id}, nil
+}
+
+func (p PaymentID) String() string {
+	return p.value.String()
+}
--- internal/domain/valueobject/yen.go
+++ internal/domain/valueobject/yen.go
@@ -1,1 +1,19 @@
 package valueobject
+
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"golang.org/x/text/number"
+)
+
+type Yen int64
+
+func NewYen(amount int) (Yen, error) {
+	if amount < 0 {
+		return 0, errors.New("amount cannot be negative")
+	}
+	return Yen(amount), nil
+}
